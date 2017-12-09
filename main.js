07f09277d22e93e3a37b20a186a80621d0029ee4
Leo Moskowitz <33180377+LeoMoskowitz@users.noreply.github.com>
2017-12-09 11:15:45 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,44 @@
-var xhttp = new XMLHttpRequest();
-var url = "https://dog.ceo/api/breeds/list"
-xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-		console.log(xhttp.response);
-		var dogDropdown = document.getElementById("dogBreed").parentNode.querySelector(".dropdown menu")
-		var dogList = JSON.parse(this.responseText);
-		for (var i = dogList.length - 1; i >= 0; i--) {
-			var itemAnchor = documentCreateElement("a");
-			itemAnchor.className = "dropdown-item";
-			itemAnchor.href = "#";
-			itemAnchor.innerText = dogList.message[i];
-			dogDropdown.appendChild(itemAnchor);
-		}
+var url= "https://dog.ceo/api/"
+function callDogAPI(url, callback){
+    var xhttp = new XMLHttpRequest();
+
+
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          // Typical action to be performed when the document is ready:
+          console.log(xhttp.response)
+            callback(this.responseText); 
+        }
+    };
+    xhttp.open("GET", url, true)
+    xhttp.send();
+}
+function createDropDown (rsp){
+    var doglist = JSON.parse(rsp)
+    var dogdropdown = document.getElementById("dogbreed").parentNode.querySelector(".dropdown-menu");
+    
+    for (var i = doglist.message.length-1; i >= 0; i--){
+        console.log(doglist.message[i])
+        var itemAnchor = document.createElement("a")
+        itemAnchor.className = "dropdown-item";
+        itemAnchor.href = "#"
+        itemAnchor.innerText = doglist.message[i]
+        dogdropdown.appendChild(itemAnchor)
     }
-};
-xhttp.open("GET", url, true);
-xhttp.send();
+}
+
+
+function createImage(rsp){
+    console.log(rsp);
+    var dogImages = JSON.parse(rsp)
+    var dogImagecont = document.getElementById("dogImage");
+    for (var i = dogImages.message. length - 1; i >= 0; i--) {
+        var dogImage = document.createElement("img")
+        dogImage.src = dogImages.message[i]
+        dogImagecont.appendChild(dogImage)
+    }
+}
+
+
+callDogAPI(url + "breed/list", createDropDown);
+callDogAPI(url + "breed/hound/images", createImage);
