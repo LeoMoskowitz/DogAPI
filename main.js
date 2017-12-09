3860aa483b3e58572c059b0c9378e0b3f68e4936
Leo Moskowitz <33180377+LeoMoskowitz@users.noreply.github.com>
2017-12-09 11:25:38 -0500

    ooops

--- main.js
+++ main.js
@@ -1,24 +1,44 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>DogAPI</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
-    
-</head>    
-<body>
-    <div class="dropdown">
-      <button class="btn btn-secondary dropdown-toggle" type="button" id="dogbreed" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        Dropdown test
-      </button>
-      <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+var url= "https://dog.ceo/api/"
+function callDogAPI(url, callback){
+    var xhttp = new XMLHttpRequest();
 
-      </div>
-        <div id="dogImage">
-            
-    </div>
-    <script type="text/javascript" src="main.js"></script>
-</body> 
-</html>
+
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          // Typical action to be performed when the document is ready:
+          console.log(xhttp.response)
+            callback(this.responseText); 
+        }
+    };
+    xhttp.open("GET", url, true)
+    xhttp.send();
+}
+function createDropDown (rsp){
+    var doglist = JSON.parse(rsp)
+    var dogdropdown = document.getElementById("dogbreed").parentNode.querySelector(".dropdown-menu");
+    
+    for (var i = doglist.message.length-1; i >= 0; i--){
+        console.log(doglist.message[i])
+        var itemAnchor = document.createElement("a")
+        itemAnchor.className = "dropdown-item";
+        itemAnchor.href = "#"
+        itemAnchor.innerText = doglist.message[i]
+        dogdropdown.appendChild(itemAnchor)
+    }
+}
+
+
+function createImage(rsp){
+    console.log(rsp);
+    var dogImages = JSON.parse(rsp)
+    var dogImagecont = document.getElementById("dogImage");
+    for (var i = dogImages.message. length - 1; i >= 0; i--) {
+        var dogImage = document.createElement("img")
+        dogImage.src = dogImages.message[i]
+        dogImagecont.appendChild(dogImage)
+    }
+}
+
+
+callDogAPI(url + "breed/list", createDropDown);
+callDogAPI(url + "breed/hound/images", createImage);
